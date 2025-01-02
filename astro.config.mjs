35e30847abc0580d48d5668418022e338522ec95
Seva Mironovich <mironovich@live.ru>
2025-01-02 19:27:55 +0300

    небольшие изменения

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,33 +6,13 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://docs.subquery.ru',
-  trailingSlash: 'always',
+  site: "https://subquery.ru",
+  base: "/docs",
+  trailingSlash: "always",
   integrations: [
     starlight({
-      title: "Дока к Platina360",
-      social: {
-        github: "https://github.com/",
-      },
-      editLink: {
-        baseUrl: 'https://github.com/withastro/starlight/edit/main/docs/',
-      },
-      customCss: [
-        './src/tailwind.css',
-      ],
-      sidebar: [
-        {
-          label: "Гайд по DBT",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Начало", slug: "dbt/start" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
+      title: "Subquery // Документация",
+      customCss: ["./src/tailwind.css"],
     }),
     tailwind({
       applyBaseStyles: false,
