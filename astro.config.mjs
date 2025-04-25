a68152363d2574eee3958cc961e6c05867cfba2b
Seva Mironovich <mironovich@live.ru>
2025-04-25 17:35:51 +0300

    add docs from margo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,43 @@
 // @ts-check
-import { defineConfig } from "astro/config"
-import starlight from "@astrojs/starlight"
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://docs.subquery.ru",
   trailingSlash: "always",
-  outDir: './dist',
+  outDir: "./dist",
   integrations: [
     starlight({
-      title: "Subquery // Документация",
+      title: "docs.subquery.ru",
       customCss: ["./src/tailwind.css"],
+      editLink: {
+        baseUrl: "https://github.com/OgnivoLab/docs/edit/master/",
+      },
+      sidebar: [
+        { label: "С чего начать", slug: "index" },
+        {
+          label: "dbt",
+          items: [
+            { label: "Что такое dbt?", slug: "dbt/start" },
+            { label: "Создание и настройка dbt проекта", slug: "dbt/project" },
+            { label: "dbt модели", slug: "dbt/models" },
+            {
+              label: "Создание и редактирование моделей в dbt",
+              slug: "dbt/models_subquery",
+            },
+            {
+              label: "Ускоряем работу с данными в SubQuery",
+              slug: "dbt/subquery_options",
+            },
+          ],
+        },
+      ],
     }),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
-})
+});
