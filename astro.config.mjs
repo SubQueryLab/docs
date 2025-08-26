ac0c08bc73e5d0c8b67a6afb5e21d4d22b554989
Seva Mironovich <mironovich@live.ru>
2025-08-26 19:45:47 +0300

    добавил счетчик метрики

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,19 @@ export default defineConfig({
       editLink: {
         baseUrl: "https://github.com/SubqueryLab/docs/edit/master/",
       },
+      head: [
+        {
+          tag: "script",
+          content: `(function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+            ym(100435056, 'init', {webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`,
+        },
+      ],
       sidebar: [
         { label: "С чего начать", slug: "index" },
         {
